62272c4f6004f65dcab526160e18a60cbd039b66
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-21 22:06:47 -0300

    iniciei o css para telas grandes

--- script/script.js
+++ script/script.js
@@ -1,10 +1,4 @@
-function mudouTamanho(){
-    if (window.innerWidth >= 768){
-        menu.style.display = 'block'
-    } else{
-        menu.style.display = 'none'
-    }
-}
+
 
 function clickMenu(){
     if(menu.style.display == 'block'){
@@ -13,3 +7,4 @@ function clickMenu(){
         menu.style.display = 'block'
     }
 }
+
