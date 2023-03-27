3997225e4ef1a51555d67fb924b5231a075d1f16
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-26 22:42:58 -0300

    continuação do media query

--- script/script.js
+++ script/script.js
@@ -1,4 +1,10 @@
-
+function mudouTamanho(){
+    if (window.innerWidth >= 768){
+        menu.style.display = 'block'
+    } else{
+        menu.style.display = 'none'
+    }
+}
 
 function clickMenu(){
     if(menu.style.display == 'block'){
